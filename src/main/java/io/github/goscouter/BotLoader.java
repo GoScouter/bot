9f437d9bee7053d757c477829880573d8b2b97c9
IdanK <me@idank.dev>
2026-07-15 16:33:44 +0300

    Add welcome message

--- src/main/java/io/github/goscouter/BotLoader.java
+++ src/main/java/io/github/goscouter/BotLoader.java
@@ -3,9 +3,11 @@ package io.github.goscouter;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import io.github.goscouter.config.Config;
 import io.github.goscouter.listeners.ReadyListener;
+import io.github.goscouter.listeners.WelcomeListener;
 import lombok.SneakyThrows;
 import net.dv8tion.jda.api.JDA;
 import net.dv8tion.jda.api.JDABuilder;
+import net.dv8tion.jda.api.requests.GatewayIntent;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
@@ -22,7 +24,8 @@ public class BotLoader {
         Config config = mapper.readValue(new File(CONFIG_FILE), Config.class);
 
         JDA jda = JDABuilder.createDefault(config.discordToken())
-                .addEventListeners(new ReadyListener(config))
+                .enableIntents(GatewayIntent.GUILD_MEMBERS)
+                .addEventListeners(new ReadyListener(config), new WelcomeListener(config))
                 .build();
 
         jda.awaitReady();
